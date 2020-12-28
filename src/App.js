60a521e6584e73946b88f1494bc92815fd87462c
PauFreixas <pfreixasmateu@gmail.com>
2020-12-28 14:34:14 +0100

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="wrapper">
+        <div id="quote-box">
+          <div className="quote-text">
+            <i className="fa fa-quote-left"> </i><span id="text"></span>
+          </div>
+          <div className="quote-author">- <span id="author"></span></div>
+          <div className="buttons">
+            <a
+                className="button"
+                id="tweet-quote"
+                title="Tweet this quote!"
+                target="_top"
+            >
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a
+                className="button"
+                id="tumblr-quote"
+                title="Post this quote on tumblr!"
+                target="_blank"
+            >
+              <i className="fa fa-tumblr"></i>
+            </a>
+            <button className="button" id="new-quote">New quote</button>
+          </div>
+        </div>
+        <div className="footer">by <a href="https://codepen.io/hezag/">hezag</a></div>
+      </div>
   );
 }
 
